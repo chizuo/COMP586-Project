aeda07edb939f36a87adb333f3ecd643e8dd695c
Jonathan <jon.chua51@gmail.com>
2022-10-21 22:58:32 -0700

    Updated fields and implemented addSection method

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,36 +1,27 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace Registration.Models
 {
     public abstract class Person
     {
         protected int Id { get; set; }
         protected string firstName { get; set; }
-        protected string? middleName { get; set; }
+        protected string middleName { get; set; }
         protected string lastName { get; set; }
         protected string gender { get; set; }
         protected int birthMonth { get; set; }
         protected int birthDay { get; set; }
         protected int birthYear { get; set; }
-        protected string? email { get; set; }
-        protected string? areaCode { get; set; }
-        protected string? phoneNumber { get; set; }
+        protected string email { get; set; }
+        protected string areaCode { get; set; }
+        protected string phoneNumber { get; set; }
         protected string address { get; set; }
         protected string city { get; set; }
         protected string state { get; set; }
         protected int zip { get; set; }
 
-        protected Dictionary<string, List<Section>> schedule = new Dictionary<string, List<Section>> {
-            { "M", new List<Section>() },
-            { "T", new List<Section>() },
-            { "W", new List<Section>() },
-            { "Th", new List<Section>() },
-            { "F", new List<Section>() },
-            { "Sa", new List<Section>() }
-        };
+        protected Dictionary<string, List<Section>> schedule;
 
         // Constructor with optional arguments
-        public Person(string firstName, string lastName, string gender, int birthMonth, int birthDay, int birthYear, string address, string city, string state, int zip, string middleName = "", string email = "", string areaCode = "", string phoneNumber = "")
+        public Person(string firstName, string lastName, string gender, int birthMonth, int birthDay, int birthYear, string address, string city, string state, int zip, Dictionary<string, List<Section>>? schedule = null, string middleName = "", string email = "", string areaCode = "", string phoneNumber = "")
         {
             this.firstName = firstName;
             this.middleName = middleName;
@@ -46,6 +37,24 @@ namespace Registration.Models
             this.email = email;
             this.areaCode = areaCode;
             this.phoneNumber = phoneNumber;
+            this.schedule = schedule != null ? schedule : new Dictionary<string, List<Section>> {
+                                                            { "M", new List<Section>() },
+                                                            { "T", new List<Section>() },
+                                                            { "W", new List<Section>() },
+                                                            { "Th", new List<Section>() },
+                                                            { "F", new List<Section>() },
+                                                            { "Sa", new List<Section>() }
+                                                        };
+        }
+
+        public void addSection(Section section)
+        {
+            var days = section.classDays;
+            foreach (string day in days)
+            {
+                List<Section> dailySchedule = schedule[day];
+                dailySchedule.Add(section);
+            }
         }
 
         public abstract Boolean availability(Section section);
