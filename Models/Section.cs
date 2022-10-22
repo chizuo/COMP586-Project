618f1a56849c5eabf1253e1ff129ee28c6a40838
Brandon-CSUN <brando.sorto.36@my.csun.edu>
2022-10-21 20:47:01 -0700

    Implemented section class

---
+++ Models/Section.cs
@@ -0,0 +1,131 @@
+namespace Registration.Models
+{
+
+    public class Section
+    {
+
+        /*
+        Course : Course Object
+        Professor : Professor Object
+        Section : int
+        Term : string
+        Year : int
+        SectionNo : int
+        Students : List of ints
+        MaxSeats : int
+        */
+        //Course courseObject = new Course();
+        //Professor professorObject = new Professer();
+
+        /*
+        Class Information:
+        Class ID Number: int
+        Class Section: int
+        School Year : int
+        School Term : string 
+        */
+
+        public int classIdNumber;
+        public int classSectionNumber;
+        public int schoolYear;
+        public string schoolTerm;
+
+        /*
+        Class Availability:
+        Enrollment Capacity: int
+        Enrollment Total: int
+        Wait List Capacity: int
+        Wait List Total: int
+        */
+
+        public int enrollmentCap;
+        public int enrollmentTotal;
+        public int waitListcap;
+        public int waitListTotal;
+
+        /*
+        Meeting Infomation:
+        Class Days: []string Mon 0, Tue 1, Wed 2, Thur 3, Friday 4, Sat 5
+        Class Time: string or DateTime object
+        
+        Start times and End times are going to be represent in a military format. 
+        7:00 in the afternoon is going to be represented as 19:00.
+        9:00 in the even is going to be represented as 21:00.
+        1:00 in the after is going to be represented as 13:00.
+        7:00 in the morning is going to be represented as 7:00.
+        Start Time: int
+        End Time: int 
+        
+
+        Class Location: string / hyperlink
+        Class Meeting Dates : string / DateTime Object 
+        */
+
+        public string[] classDays;
+        public int startTime;
+
+        public int endTime;
+        public string classLocation;
+        public string classMeetingDates;
+
+
+        /*
+        Notes:
+        Class Note : string
+        */
+        public string classNote;
+
+        // Still waiting on the students class
+        //public List<students> studentsList;
+
+        /*
+        Add Numbers: Dictionary<int, string>
+        */
+
+        public Dictionary<int, string> addNumbers;
+
+
+        public Section(int classIdNumber, int classSectionNumber, string schoolTerm, int schoolYear, int enrollmentCap, int enrollmentTotal, int waitListcap, int waitListTotal, string[] classDays, int startTime, int endTime, string classLocation, string classMeetingDates, string classNote, Dictionary<int, string> addNumbers)
+        {
+            this.classIdNumber = classIdNumber;
+            this.classSectionNumber = classSectionNumber;
+
+            this.schoolTerm = schoolTerm;
+            this.schoolYear = schoolYear;
+
+            this.enrollmentCap = enrollmentCap;
+            this.enrollmentTotal = enrollmentTotal;
+            this.waitListcap = waitListcap;
+            this.waitListTotal = waitListTotal;
+
+            this.classDays = classDays;
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.classLocation = classLocation;
+            this.classMeetingDates = classMeetingDates;
+
+            this.classNote = classNote;
+
+            this.addNumbers = addNumbers;
+        }
+
+             /*public void createAddNumbers()
+            {
+            Random random = new Random();
+            int addnumber = random.Next(10000, 99999);
+            for (var i = 0; i < addNumbers.Count; i++)
+            {
+                if (addnumber != addNumbers[i])
+                {
+                    addNumbers.Add(addnumber);
+                    break;
+                }
+                else
+                {
+                    createAddNumbers();
+                }
+            }
+            Console.WriteLine(addnumber);
+        }*/
+    }
+}
