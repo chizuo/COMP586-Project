a32af8e735cd6e240ef7a2ef3065ffac580261dd
Sadhnani <harshleen.sadhnani.082@my.csun.edu>
2022-10-21 21:27:48 -0700

    Implemented Student class

---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Registration.Models
+{
+    public class Student : Person
+    {
+
+        //key: course name, value: credits, grade
+        Dictionary<string, Tuple<int, float>> courses { get; set; };
+
+        public void calculateGPA()
+        {
+            
+        }
+    }
