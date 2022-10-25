a4207c5b362cb4631fa70c6198037fea44805769
Brandon-CSUN <brando.sorto.36@my.csun.edu>
2022-10-24 23:24:01 -0700

    implemnet the generate add number method and derive full course name

--- Models/Course.cs
+++ Models/Course.cs
@@ -5,7 +5,8 @@ namespace Registration.Models
         /*
         Dept: COMP
         CourseNum: 586
-        Name: concat Dept & CourseNum
+        Course Name: Object-Oriented Software Development
+        Full Course Name : COMP 586 Object-Oriented Software Development
         Description: string
         PreReq: list of strings
         CoReq: list of strings
@@ -14,15 +15,17 @@ namespace Registration.Models
         public string deptName;
         public int courseNumber;
         public string courseName;
+        public string fullCourseName;
         public string courseDescription;
         public List<Course> preRequisites;
         public List<Course> coRequisites;
 
-        public Course(string deptName, int courseNumber, string courseName, string courseDescription, List<Course> preReq, List<Course> coReq)
+        public Course(string deptName, int courseNumber, string courseName, string fullCourseName, string courseDescription, List<Course> preReq, List<Course> coReq)
         {
             this.deptName = deptName;
             this.courseNumber = courseNumber;
             this.courseName = courseName;
+            this.fullCourseName = deptName + " " + courseNumber + " " + courseName;
             this.courseDescription = courseDescription;
             this.preRequisites = preReq;
             this.coRequisites = coReq;
