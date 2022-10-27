e22b375cefdffa8d5bf96d5a41d6b3c19a42daf2
Jacob-Asaad <89406161+Jacob-Asaad@users.noreply.github.com>
2022-10-26 22:22:56 -0700

    removeSection Added

--- Models/Professor.cs
+++ Models/Professor.cs
@@ -6,27 +6,5 @@ namespace Registration.Models
          : base(firstName, lastName, gender, birthMonth, birthDay, birthYear, address, city, state, zip, personType, schedule = null, middleName = "", email = "", areaCode = "", phoneNumber = "")
         {
         }
-        public Boolean removeSection(Section section){
-           int functionCheck=0; //incremented each time a section is removed 
-           int sectionCode = section.classSectionNumber; //retrieving the section's section number 
-           string [] daysOfOperation = section.classDays; //retrieving the days for sections
-            
-            foreach(string courseDay in daysOfOperation){ //for each day that the section occurs
-                List<Section> dailySchedule = schedule[courseDay]; //retrieving the scheduled days the section occurs on
-                int index = 0;
-                foreach(Section sections in dailySchedule){ //iterating through the persons schedule of sections in each day that contains the section that needs to be removed
-  
-                    if(sections.classSectionNumber==sectionCode){ //if that day contains the section number we want -> remove it, otherwise break, and check the next day (index)
-                        dailySchedule.RemoveAt(index); 
-                        functionCheck++;
-                        break;
-                    }
-                    index++;
-                }
-            }
-            if(functionCheck==daysOfOperation.Length){return true;} //when the functionChecker is equal to the number of sections removed -> 
-                                                                    //return true, ensuring successful removal of the section passed 
-            return false; //this function should never return false
-        }
     }
 }
