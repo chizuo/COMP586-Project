bd8a2f9090e7fc074434a44fdfbdda263439c773
Jonathan <jon.chua51@gmail.com>
2022-10-27 11:34:19 -0700

    changed semesterSection to a Dictionary from List

--- Models/Professor.cs
+++ Models/Professor.cs
@@ -2,12 +2,12 @@ namespace Registration.Models
 {
     public class Professor : Person
     {
-        List<Section>? semesterSections;
+        Dictionary<string, Section>? semesterSections;
 
-        public Professor(string firstName, string lastName, string gender, int birthMonth, int birthDay, int birthYear, string address, string city, string state, int zip, string personType, Dictionary<string, List<Section>>? schedule = null, string middleName = "", string email = "", string areaCode = "", string phoneNumber = "", List<Section>? semesterSections = null)
+        public Professor(string firstName, string lastName, string gender, int birthMonth, int birthDay, int birthYear, string address, string city, string state, int zip, string personType, Dictionary<string, List<Section>>? schedule = null, string middleName = "", string email = "", string areaCode = "", string phoneNumber = "", Dictionary<string, Section>? semesterSections = null)
          : base(firstName, lastName, gender, birthMonth, birthDay, birthYear, address, city, state, zip, personType, schedule = null, middleName = "", email = "", areaCode = "", phoneNumber = "")
         {
-            this.semesterSections = semesterSections != null ? semesterSections : new List<Section>();
+            this.semesterSections = semesterSections != null ? semesterSections : new Dictionary<string, Section>();
         }
     }
 }
