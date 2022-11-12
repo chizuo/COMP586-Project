18d8233523ec580dbd7834530d49f0cdd702902e
Jonathan <jon.chua51@gmail.com>
2022-11-11 17:28:54 -0800

    stash

--- Controllers/ProfessorController.cs
+++ Controllers/ProfessorController.cs
@@ -1,14 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
+using Registration.Models;
 
 namespace Registration.Controllers
 {
     public class ProfessorController : Controller
     {
+        Dictionary<string, string> db_login = new Dictionary<string, string>();
+        Dictionary<string, Professor> db_users = new Dictionary<string, Professor>();
+
         // GET: /Professor/
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public void Index(string id, string password)
+        {
+            Welcome();
         }
 
         // GET: /Professor/Welcome/
--- Models/Professor.cs
+++ Models/Professor.cs
@@ -2,7 +2,7 @@ namespace Registration.Models
 {
     public class Professor : Person
     {
-        Dictionary<string, Section>? cachedSections;
+        Dictionary<string, Section> cachedSections;
 
         public Professor(string firstName, string lastName, string gender, int birthMonth, int birthDay, int birthYear, string address, string city, string state, int zip, string personType, Dictionary<string, List<Section>>? schedule = null, string middleName = "", string email = "", string areaCode = "", string phoneNumber = "", Dictionary<string, Section>? cachedSections = null)
          : base(firstName, lastName, gender, birthMonth, birthDay, birthYear, address, city, state, zip, personType, schedule = null, middleName = "", email = "", areaCode = "", phoneNumber = "")
