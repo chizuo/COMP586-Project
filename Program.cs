853cfb467f45f06ad710851f6428e1aa99fc72cb
Jacob-Asaad <89406161+Jacob-Asaad@users.noreply.github.com>
2022-11-23 13:03:40 -0800

    Review Request - FIXED

--- Program.cs
+++ Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Professor}/{action=index}/{id?}");
+    pattern: "{controller=Home}/{action=index}/{id?}");
 
 app.Run();
