588069e78a7aa82407f3db173470dd278fa72630
Brandon Sorto <78838697+Brandon-CSUN@users.noreply.github.com>
2022-12-10 23:52:48 -0800

    added Student View and Controller and NewDb Schema picture

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Registration.Controllers
 {
-    public class ProfessorController : Controller
+    public class StudentController : Controller
     {
         // GET: /Professor/
         public ActionResult index()
@@ -30,12 +30,12 @@ namespace Registration.Controllers
                 {
         
                     var personRespone = db.dbPerson.Single(p => p.id == login.id);
-                    Professor professor = new Professor(personRespone.id, personRespone.first, personRespone.last, personRespone.Gender, personRespone.birthMonth, personRespone.birthDay, personRespone.birthYear,
-                                                        personRespone.address, personRespone.city, personRespone.state, personRespone.zip, personRespone.personType, null,
-                                                        personRespone.middle, personRespone.email, personRespone.areaCode, personRespone.phone);
-
+                    Student student = new Student(personRespone.id, personRespone.first, personRespone.last, personRespone.Gender, personRespone.birthMonth, personRespone.birthDay, personRespone.birthYear,
+                                                        personRespone.address, personRespone.city, personRespone.state, personRespone.zip, personRespone.personType, null, null,
+                                                        personRespone.middle, personRespone.email, personRespone.areaCode, personRespone.phone, null);
+                    //
                     var departmentRespone = db.dbDepartments.Single(d => d.code == "COMP");
-                    Department department = new Department(departmentRespone.name, departmentRespone.code, professor);
+                    Department department = new Department(departmentRespone.name, departmentRespone.code, null);
 
                     var scheduleRespone = db.dbEnrollmentSchedules.AsNoTracking().Where(e => e.id == login.id);
 
@@ -46,12 +46,15 @@ namespace Registration.Controllers
                         var sectionRespone = db.dbSections.AsNoTracking().Where(s => s.sectionNumber == schedule.sectionNumber);
                         foreach (var section in sectionRespone)
                         {
+                            List<Section> sections = new List<Section>();
+                            
                             var classDaysRespone = db.dbClassDays.AsNoTracking().Where(d => d.sectionNumber == section.sectionNumber).ToArray();
                             string[] classDays = new string[classDaysRespone.Length];
                             for(int i = 0; i < classDays.Length; i++)
                             {
                                 classDays[i] = classDaysRespone[i].class_day;
                             }
+
                             Console.WriteLine(section.schoolTerm);
                             var courseRespone = db.dbCourses.AsNoTracking().Where(c => c.course_Id == section.course_Id);
                             foreach (var course in courseRespone)
@@ -59,11 +62,17 @@ namespace Registration.Controllers
                                 Console.WriteLine(course.number);
                                 Course courseObject = new Course(department, course.number, course.subject, course.units, course.description, new HashSet<string>(), new HashSet<string>(), course.isLab);
                                 Section sectionObject = new Section(courseObject, section.sectionNumber, section.schoolTerm, section.schoolYear, section.enrollmentCap, section.waitListcap, section.waitListTotal, classDays, section.startTime, section.endTime, section.classLocation, null, section.classNote, null, null);
-                                professor.Sections.Add(sectionObject);
+                                student.Sections.Add(sectionObject);
+                                /*
+                                for(int i = 0; i < classDays.Length; i++)
+                                {
+                                    student.Schedule.Add(classDays[i], sections);
+                                }
+                                */
                             }
                         }   
                     }
-                    return View("Dashboard", professor);
+                    return View("Dashboard", student);
                 }
             }
         }
