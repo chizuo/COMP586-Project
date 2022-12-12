676520dcc6a9047641e3399ad2c0bc1d6f18b5e1
Brandon-CSUN <brando.sorto.36@my.csun.edu>
2022-12-11 21:56:46 -0800

    Student view updates

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -28,14 +28,11 @@ namespace Registration.Controllers
 
                 else
                 {
-        
+
                     var personRespone = db.dbPerson.Single(p => p.id == login.id);
                     Student student = new Student(personRespone.id, personRespone.first, personRespone.last, personRespone.Gender, personRespone.birthMonth, personRespone.birthDay, personRespone.birthYear,
                                                         personRespone.address, personRespone.city, personRespone.state, personRespone.zip, personRespone.personType, null, null,
                                                         personRespone.middle, personRespone.email, personRespone.areaCode, personRespone.phone, null);
-                    //
-                    var departmentRespone = db.dbDepartments.Single(d => d.code == "COMP");
-                    Department department = new Department(departmentRespone.name, departmentRespone.code, null);
 
                     var scheduleRespone = db.dbEnrollmentSchedules.AsNoTracking().Where(e => e.id == login.id);
 
@@ -43,14 +40,24 @@ namespace Registration.Controllers
                     {
                         Console.WriteLine(schedule.sectionNumber);
 
+
+
                         var sectionRespone = db.dbSections.AsNoTracking().Where(s => s.sectionNumber == schedule.sectionNumber);
                         foreach (var section in sectionRespone)
                         {
                             List<Section> sections = new List<Section>();
-                            
+
+                            var departmentRespone = db.dbDepartments.Single(d => d.code == section.dept);
+                            Department department = new Department(departmentRespone.name, departmentRespone.code, null);
+
+                            var professorRespone = db.dbPerson.Single(p => p.id == section.professor);
+                            Professor professor = new Professor(professorRespone.id, professorRespone.first, professorRespone.last, professorRespone.Gender, professorRespone.birthMonth, professorRespone.birthDay, professorRespone.birthYear,
+                                                                professorRespone.address, professorRespone.city, professorRespone.state, professorRespone.zip, professorRespone.personType, null,
+                                                                professorRespone.middle, professorRespone.email, professorRespone.areaCode, professorRespone.phone);
+
                             var classDaysRespone = db.dbClassDays.AsNoTracking().Where(d => d.sectionNumber == section.sectionNumber).ToArray();
                             string[] classDays = new string[classDaysRespone.Length];
-                            for(int i = 0; i < classDays.Length; i++)
+                            for (int i = 0; i < classDays.Length; i++)
                             {
                                 classDays[i] = classDaysRespone[i].class_day;
                             }
@@ -61,16 +68,17 @@ namespace Registration.Controllers
                             {
                                 Console.WriteLine(course.number);
                                 Course courseObject = new Course(department, course.number, course.subject, course.units, course.description, new HashSet<string>(), new HashSet<string>(), course.isLab);
-                                Section sectionObject = new Section(courseObject, section.sectionNumber, section.schoolTerm, section.schoolYear, section.enrollmentCap, section.waitListcap, section.waitListTotal, classDays, section.startTime, section.endTime, section.classLocation, null, section.classNote, null, null);
+                                Section sectionObject = new Section(courseObject, section.sectionNumber, section.schoolTerm, section.schoolYear, section.enrollmentCap, section.waitListcap, section.waitListTotal, classDays, section.startTime, section.endTime, section.classLocation, null, section.classNote, null, professor);
                                 student.Sections.Add(sectionObject);
-                                /*
-                                for(int i = 0; i < classDays.Length; i++)
+
+                                for (int i = 0; i < classDays.Length; i++)
                                 {
-                                    student.Schedule.Add(classDays[i], sections);
+                                    Console.WriteLine(classDays[i]);
+                                    student.Schedule[classDays[i]] = sections;
                                 }
-                                */
+
                             }
-                        }   
+                        }
                     }
                     return View("Dashboard", student);
                 }
