b8bcec694a806228efd7ae85ee17060d6164a7bc
Jacob-Asaad <89406161+Jacob-Asaad@users.noreply.github.com>
2022-12-11 20:57:58 -0800

    General Front End Routes & Improvements

--- Program.cs
+++ Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Student}/{action=Dashboard}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
