43e918af2f5dd07b9549c82af707ef9634c572db
Brandon-CSUN <brando.sorto.36@my.csun.edu>
2022-12-13 01:11:44 -0800

    Updated Version

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -11,14 +11,14 @@ namespace Registration.Controllers
         static HashSet<string> pre = new HashSet<string>();
         static HashSet<string> co = new HashSet<string>();
 
-        public ActionResult index(string? course = null)
+        public ActionResult index(string? course = "COMP380")
         {
             using (var db = new Context())
             {
-                var courseRespone = db.dbCourses.Single(c => c.course_Id == "COMP380");
+                var courseRespone = db.dbCourses.Single(c => c.course_Id == course);
                 var deptRespone = db.dbDepartments.Single(d => d.code == courseRespone.department);
-                var preRespone = db.dbPreReqs.AsNoTracking().Where(pre => pre.course_Id == "COMP380");
-                var coRespone = db.dbCoReqs.AsNoTracking().Where(core => core.course_Id == "COMP380");
+                var preRespone = db.dbPreReqs.AsNoTracking().Where(pre => pre.course_Id == course);
+                var coRespone = db.dbCoReqs.AsNoTracking().Where(core => core.course_Id == course);
 
                 Department Comp = new Department(deptRespone.name, deptRespone.code, null);
                 Course courseObject = new Course(Comp, courseRespone.number, courseRespone.subject, courseRespone.units, courseRespone.description, new HashSet<string>(), new HashSet<string>(), courseRespone.isLab);
@@ -50,22 +50,62 @@ namespace Registration.Controllers
 
         }
 
-        public ActionResult Qualify(string? studentid, string? courseid)
+        public ActionResult qualify(int? studentid = 1234, string? courseid = "COMP610")
         {
             using (var db = new Context())
             {
-                var courseRespone = db.dbCourses.AsNoTracking().Where(c => c.course_Id == "COMP380");
-                var studentRespone = db.dbStudents.AsNoTracking().Where(s => s.student_id == studentid);
 
-                foreach (var courses in courseRespone)
-                {
-                    Console.WriteLine(courses.subject);
-                }
+                var courseRespone = db.dbCourses.Single(c => c.course_Id == courseid);
+                var preRespone = db.dbPreReqs.AsNoTracking().Where(pre => pre.course_Id == courseid);
+                var coRespone = db.dbCoReqs.AsNoTracking().Where(core => core.course_Id == courseid);
 
-                foreach (var student in studentRespone)
+                var deptRespone = db.dbDepartments.Single(d => d.code == courseRespone.department);
+                Department Comp = new Department(deptRespone.name, deptRespone.code, null);
+
+                Course course = new Course(Comp, courseRespone.number, courseRespone.subject, courseRespone.units, courseRespone.description, new HashSet<string>(), new HashSet<string>(), courseRespone.isLab);
+
+                foreach (var preReq in preRespone)
                 {
-                    Console.WriteLine(student.grade);
+                    var prerequisites = db.dbCourses.AsNoTracking().Where(pre => pre.course_Id == preReq.prereq_Id);
+                    foreach (var preCourse in prerequisites)
+                    {
+                        pre.Add(preCourse.course_Id);
+                    }
                 }
+                course.PreRequisites = pre;
+
+                foreach (var coReq in coRespone)
+                {
+                    var corequisites = db.dbCourses.AsNoTracking().Where(co => co.course_Id == coReq.coreq_Id);
+                    foreach (var coCourse in corequisites)
+                    {
+                        co.Add(coCourse.course_Id);
+                    }
+                }
+                course.CoRequisites = co;
+
+                var sectionRespone = db.dbSections.Single(s => s.sectionNumber == 10007);
+                var gradeRespone = db.dbStudents.Single(g => g.student_id == studentid.ToString());
+                var courseRes = db.dbCourses.Single(c => c.course_Id == gradeRespone.course_id);
+
+                Course courseobject = new Course(Comp, courseRes.number, courseRes.subject, courseRes.units, courseRes.description, new HashSet<string>(), new HashSet<string>(), courseRes.isLab);
+
+                //Section section = new Section(course, sectionRespone.sectionNumber, sectionRespone.schoolTerm, sectionRespone.schoolYear, sectionRespone.enrollmentCap, sectionRespone.waitListcap, sectionRespone.waitListTotal, null, sectionRespone.startTime, sectionRespone.endTime, sectionRespone.classLocation, null, sectionRespone.classNote, null, null);
+                Console.WriteLine("Course Name - " + gradeRespone.course_id);
+
+                List<Tuple<Course, double>> trans = new List<Tuple<Course, double>>();
+                trans.Add(Tuple.Create(courseobject, (double)gradeRespone.grade));
+
+                var personRespone = db.dbPerson.Single(s => s.id == studentid);
+
+                Student student = new Student(personRespone.id, personRespone.first, personRespone.last, personRespone.Gender, personRespone.birthMonth, personRespone.birthDay, personRespone.birthYear,
+                                                        personRespone.address, personRespone.city, personRespone.state, personRespone.zip, personRespone.personType, trans, null,
+                                                        personRespone.middle, personRespone.email, personRespone.areaCode, personRespone.phone, null);
+
+
+                //student.Transcript.Add(Tuple.Create(section, (double)gradeRespone.grade))
+
+                Console.WriteLine(course.requirements(student));
             }
 
             return View();
--- Models/Student.cs
+++ Models/Student.cs
@@ -2,16 +2,16 @@ namespace Registration.Models
 {
     public class Student : Person
     {
-        protected List<Tuple<Section, double>> transcript;
-        public List<Tuple<Section, double>> Transcript { get; set; }
+        protected List<Tuple<Course, double>> transcript;
+        public List<Tuple<Course, double>> Transcript { get; set; }
         public double GPA { get { return overallGPA(); } }
         protected List<Section> cachedSections;
         public List<Section> Sections { get { return cachedSections; } }
 
-        public Student(int id, string first, string last, string gender, int birthMonth, int birthDay, int birthYear, string address, string city, string state, string zip, string personType, List<Tuple<Section, double>>? transcript = null, Dictionary<string, List<Section>>? schedule = null, string middle = "", string email = "", string areaCode = "", string phone = "",List<Section>? cachedSections = null)
+        public Student(int id, string first, string last, string gender, int birthMonth, int birthDay, int birthYear, string address, string city, string state, string zip, string personType, List<Tuple<Course, double>>? transcript = null, Dictionary<string, List<Section>>? schedule = null, string middle = "", string email = "", string areaCode = "", string phone = "", List<Section>? cachedSections = null)
          : base(id, first, last, gender, birthMonth, birthDay, birthYear, address, city, state, zip, personType, schedule = null, middle, email, areaCode, phone)
         {
-            this.transcript = transcript == null ? new List<Tuple<Section, double>>() : transcript;
+            this.transcript = transcript == null ? new List<Tuple<Course, double>>() : transcript;
             this.cachedSections = cachedSections != null ? cachedSections : new List<Section>();
         }
 
@@ -21,7 +21,7 @@ namespace Registration.Models
             int unitsTaken = 0;
             foreach (var grade in transcript)
             {
-                unitsTaken += grade.Item1.getUnits();
+                unitsTaken += grade.Item1.Units;
                 gradePoints += grade.Item2;
             }
             return Math.Truncate((gradePoints / unitsTaken) * 100) / 100;
@@ -31,10 +31,14 @@ namespace Registration.Models
         public List<string> coursesPassed()
         {
             var courseList = new List<string>();
-            foreach (var section in this.transcript)
+            foreach (var course in transcript)
             {
-                if (section.Item2 >= 2.0)
-                    courseList.Add(section.Item1.getCourseName());
+                if (course.Item2 >= 2.0)
+                {
+                    courseList.Add(course.Item1.Code);
+                    Console.Write("Course passed - ");
+                    Console.WriteLine(course.Item1.Code);
+                }
             }
             return courseList;
         }
